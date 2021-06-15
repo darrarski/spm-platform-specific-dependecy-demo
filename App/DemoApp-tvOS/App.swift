a536f557d5ae990b31ba9e6da8770bce3f028bb0
Dariusz Rybicki <darrarski@gmail.com>
2021-06-15 22:20:38 +0200

    Embed AppFeature in Demo apps

--- App/DemoApp-tvOS/App.swift
+++ App/DemoApp-tvOS/App.swift
@@ -1,11 +1,11 @@
+import AppFeature
 import SwiftUI
 
 @main
 struct App: SwiftUI.App {
   var body: some Scene {
     WindowGroup {
-      Text("Hello, world!")
-        .padding()
+      AppView()
     }
   }
 }
